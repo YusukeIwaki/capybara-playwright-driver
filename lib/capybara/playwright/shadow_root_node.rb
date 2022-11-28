b01bda09dbc71069608d41bcba991ad07900419f
YusukeIwaki <iwaki@i3-systems.com>
2022-11-29 03:05:33 +0900

    fix shadow_root specs to pass.

--- lib/capybara/playwright/shadow_root_node.rb
+++ lib/capybara/playwright/shadow_root_node.rb
@@ -9,30 +9,31 @@ module Capybara
       end
 
       def all_text
-        assert_element_not_stale
-
-        text = @shadow_roow_element.text_content
-        text.to_s.gsub(/[\u200b\u200e\u200f]/, '')
-            .gsub(/[\ \n\f\t\v\u2028\u2029]+/, ' ')
-            .gsub(/\A[[:space:]&&[^\u00a0]]+/, '')
-            .gsub(/[[:space:]&&[^\u00a0]]+\z/, '')
-            .tr("\u00a0", ' ')
+        assert_element_not_stale {
+          text = @shadow_roow_element.text_content
+          text.to_s.gsub(/[\u200b\u200e\u200f]/, '')
+              .gsub(/[\ \n\f\t\v\u2028\u2029]+/, ' ')
+              .gsub(/\A[[:space:]&&[^\u00a0]]+/, '')
+              .gsub(/[[:space:]&&[^\u00a0]]+\z/, '')
+              .tr("\u00a0", ' ')
+        }
       end
 
       def visible_text
-        assert_element_not_stale
+        assert_element_not_stale {
 
-        return '' unless visible?
+          return '' unless visible?
 
-        # https://github.com/teamcapybara/capybara/blob/1c164b608fa6452418ec13795b293655f8a0102a/lib/capybara/rack_test/node.rb#L18
-        displayed_text = @shadow_roow_element.text_content.to_s.
-                            gsub(/[\u200b\u200e\u200f]/, '').
-                            gsub(/[\ \n\f\t\v\u2028\u2029]+/, ' ')
-        displayed_text.squeeze(' ')
-          .gsub(/[\ \n]*\n[\ \n]*/, "\n")
-          .gsub(/\A[[:space:]&&[^\u00a0]]+/, '')
-          .gsub(/[[:space:]&&[^\u00a0]]+\z/, '')
-          .tr("\u00a0", ' ')
+          # https://github.com/teamcapybara/capybara/blob/1c164b608fa6452418ec13795b293655f8a0102a/lib/capybara/rack_test/node.rb#L18
+          displayed_text = @shadow_roow_element.text_content.to_s.
+                              gsub(/[\u200b\u200e\u200f]/, '').
+                              gsub(/[\ \n\f\t\v\u2028\u2029]+/, ' ')
+          displayed_text.squeeze(' ')
+            .gsub(/[\ \n]*\n[\ \n]*/, "\n")
+            .gsub(/\A[[:space:]&&[^\u00a0]]+/, '')
+            .gsub(/[[:space:]&&[^\u00a0]]+\z/, '')
+            .tr("\u00a0", ' ')
+        }
       end
     end
   end
