8564540753f648d33eced5fe315c7763ed3fc71d
YusukeIwaki <iwaki@i3-systems.com>
2026-08-16 17:43:08 +0900

    Fix negative number field input

--- spec/compatibility/fill_in_spec.rb
+++ spec/compatibility/fill_in_spec.rb
@@ -17,6 +17,10 @@ FILL_IN_FORM_HTML = <<~HTML
         Date
         <input type="date" name="form[date]" id="form_date"/>
       </label>
+      <label for="form_amount">
+        Amount
+        <input type="number" name="form[amount]" id="form_amount"/>
+      </label>
     </form>
     <input type="text" name="with_change_event" value="default value" id="with_change_event"/>
     <input type="text" name="with_focus_event" value="" id="with_focus_event"/>
@@ -74,6 +78,15 @@ FILL_IN_HELPERS = Module.new do
     JS
     find(:css, 'h1', text: 'Form').click
   end
+
+  def track_form_amount_keys
+    find(:css, '#form_amount').execute_script <<~JS
+      window.capybara_formAmountKeydownEvents = [];
+      this.addEventListener('keydown', function(event) {
+        window.capybara_formAmountKeydownEvents.push(event.key);
+      });
+    JS
+  end
 end
 
 RSpec.describe 'fill_in compatibility', sinatra: true do
@@ -110,6 +123,19 @@ RSpec.describe 'fill_in compatibility', sinatra: true do
     fill_in('form_first_name', with: 'Harry', fill_options: { clear: :backspace })
 
     expect(find(:fillable_field, 'form_first_name').value).to eq('Harry')
+  end
+
+  it 'fills number values with incomplete prefixes' do
+    visit '/'
+    track_form_amount_keys
+
+    %w[-7 .5 1e3].each do |value|
+      fill_in('form_amount', with: value)
+
+      expect(find(:fillable_field, 'form_amount').value).to eq(value)
+    end
+
+    expect(evaluate_script('window.capybara_formAmountKeydownEvents')).to eq %w[- 7 . 5 1 e 3]
   end
 
   it 'triggers onchange once' do
--- spec/compatibility/node/set_spec.rb
+++ spec/compatibility/node/set_spec.rb
@@ -8,14 +8,18 @@ SET_TAB_KEY_HTML = <<~HTML
   <body>
     <label for="tag">Tag</label>
     <input id="tag" name="tag">
+    <label for="amount">Amount</label>
+    <input id="amount" name="amount" type="number">
     <div id="status">waiting</div>
     <script>
-      tag.addEventListener('keydown', function(event) {
-        if (event.key === 'Tab') {
-          event.preventDefault();
-          document.getElementById('status').textContent = 'tabbed:' + tag.value;
-          alert('tab was pressed');
-        }
+      [tag, amount].forEach(function(input) {
+        input.addEventListener('keydown', function(event) {
+          if (event.key === 'Tab') {
+            event.preventDefault();
+            document.getElementById('status').textContent = 'tabbed:' + input.value;
+            alert('tab was pressed');
+          }
+        });
       });
     </script>
   </body>
@@ -35,5 +39,17 @@ RSpec.describe 'Node#set compatibility', sinatra: true do
     expect(message).to eq('tab was pressed')
     expect(find('#status')).to have_text('tabbed:bad tag')
     expect(find('#tag').value).to eq('bad tag')
+  end
+
+  it 'presses Tab when setting a number input' do
+    visit '/tab-key'
+
+    message = accept_alert('tab was pressed', wait: 0.5) do
+      find('#amount').set("12\t")
+    end
+
+    expect(message).to eq('tab was pressed')
+    expect(find('#status')).to have_text('tabbed:12')
+    expect(find('#amount').value).to eq('12')
   end
 end
